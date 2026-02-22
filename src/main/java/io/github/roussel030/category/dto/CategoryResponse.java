1a9c940344e5fda1a6d11ab5090241192376052e
Roussel-030 <yvanroussel31@gmail.com>
2026-02-22 09:01:44 +0300

    Add documentation open api for category

--- src/main/java/io/github/roussel030/category/dto/CategoryResponse.java
+++ src/main/java/io/github/roussel030/category/dto/CategoryResponse.java
@@ -1,6 +1,17 @@
 package io.github.roussel030.category.dto;
 
-public record CategoryResponse(Long id, String name) {
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name = "CategoryResponse", description = "Response returned for a house category")
+public record CategoryResponse(
+
+        @Schema(description = "Unique ID of the house category", example = "1")
+        Long id,
+
+        @Schema(description = "Name of the house category", example = "Living Room")
+        String name
+
+) {
 
     public static Builder builder() {
         return new Builder();
